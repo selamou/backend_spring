a33d03e909ca214c3812861f1c61db8bf788489b
selamou <selamoumouhamedou@gmail.com>
2023-02-14 11:44:52 +0000

    bla

---
+++ src/main/java/com/projetspring/meriem/entities/OrderForm.java
@@ -0,0 +1,11 @@
+package com.projetspring.meriem.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+@Data
+public class OrderForm {
+    private Client client=new Client();
+    private List<OrderProduct> products=new ArrayList<>();
+}
